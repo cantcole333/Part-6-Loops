995e3ab65b9bbede88a0ae7febd94c3af975ffb6
Cole Cantwell <cole>
2020-10-07 22:51:32 -0400

    i tried at loops

--- Part 6/Program.cs
+++ Part 6/Program.cs
@@ -10,10 +10,18 @@ namespace Part_6
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 9; i+= 2)
-            {
-                Console.Write($"{i} ");
-            }
+            int min;
+            int max;
+            int userNum;
+            Console.WriteLine("Please enter a minimum integer value");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a maximum integer value");
+            max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The range is {min} to {max}");
+            Console.WriteLine("Enter a number in the range");
+            while (!Int32.TryParse(Console.ReadLine(), out userNum))
+                Console.WriteLine("That is outside the range, please try again");
+            Console.WriteLine("thanks!");
             Console.ReadLine();
         }
     }
